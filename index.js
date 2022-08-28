c4b8256b44519523b214053fd40407fbfd3e7466
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-28 20:59:21 +0430

    add public folder

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const routes = require('./routes.js')
+const path = require('path')
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, './public')))
 app.use(express.json())
 app.use(express.urlencoded())
 app.use(routes)

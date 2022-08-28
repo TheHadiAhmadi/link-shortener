41612b59618dd016f879d551abea07fc133f72b7
TheHadiAhmadi <thehadiahmadi@gmail.com>
2022-08-28 20:35:11 +0430

    update

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./index.js')
-// require('dotenv').config()
+require('dotenv').config()
 
 const port = process.env.PORT || 3000
 app.listen(port, () => console.log("app is listening on port " + port))
